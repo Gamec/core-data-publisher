42a1fe0c19bcc33064a707e412e9f068e15585f3
Paweł Burda <pburda@gmail.com>
2026-01-28 08:44:05 +0100

    One more threading fix

--- Sources/CoreDataPublisher/FetchedResultsSubscription.swift
+++ Sources/CoreDataPublisher/FetchedResultsSubscription.swift
@@ -20,8 +20,8 @@ final class FetchedResultsSubscription<SubscriberType: Subscriber, Entity: NSMan
     }
 
     func request(_: Subscribers.Demand) {
-        if let entities = fetchedResultsController?.fetchedObjects {
-            managedObjectContext?.performAndWait {
+        managedObjectContext?.performAndWait {
+            if let entities = fetchedResultsController?.fetchedObjects {
                 _ = subscriber?.receive(entities)
             }
         }
@@ -49,13 +49,15 @@ final class FetchedResultsSubscription<SubscriberType: Subscriber, Entity: NSMan
         fetchedResultsController.delegate = self
         self.fetchedResultsController = fetchedResultsController
 
-        do {
-            try fetchedResultsController.performFetch()
-            if let entities = fetchedResultsController.fetchedObjects {
-                _ = subscriber?.receive(entities)
+        managedObjectContext.performAndWait {
+            do {
+                try fetchedResultsController.performFetch()
+                if let entities = fetchedResultsController.fetchedObjects {
+                    _ = subscriber?.receive(entities)
+                }
+            } catch {
+                assertionFailure(error.localizedDescription)
             }
-        } catch {
-            assertionFailure(error.localizedDescription)
         }
     }
 
